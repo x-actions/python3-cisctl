cb4d414368b467e38d3d9785f717d1ca87b53f15
xiexianbin <me@xiexianbin.cn>
2022-04-01 22:36:07 +0800

    support gcr image with /cmd/

--- cisctl/tests/unit/test_utils.py
+++ cisctl/tests/unit/test_utils.py
@@ -23,6 +23,10 @@ class UtilsTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_date2timestamp(self):
+    def test_date2timestamp(self):  # noqa
         date_time = '2020-06-28T11:46:53.539425Z'
         print(utils.date2timestamp(date_time))
+
+    def test_generate_dest_name(self):  # noqa
+        print(utils.generate_dest_name('gcr.io/knative-releases/knative.dev/eventing/cmd', 'appender'))
+        print(utils.generate_dest_name('gcr.io/tekton-releases/github.com/tektoncd/triggers/cmd', 'webhook'))
