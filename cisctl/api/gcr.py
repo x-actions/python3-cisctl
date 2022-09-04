bccbe59d58a3f1a5ca1ff44d550b0e86f53ffdf3
xiexianbin <me@xiexianbin.cn>
2022-09-04 21:44:19 +0800

    fix knative cmd two slash bug (#8)
    1. e.g. ...eventing/cmd/in_memory/channel_controller
    
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

--- cisctl/api/gcr.py
+++ cisctl/api/gcr.py
@@ -85,6 +85,9 @@ class GoogleContainerRegisterV2(RegisterBaseAPIV2):
             _tag_dict = {}
             for digest, v in resp.get('manifest', {}).items():
                 for tag_name in v['tag']:
+                    # tag like sha256-7939f3c6366155e73cb5025fc1fde5bd70c350cc9cd6b505340f7db8af655832.sbom is un-valid
+                    if tag_name.startswith('sha256-') and tag_name.endswith('.sbom'):
+                        continue
                     _tag_dict[tag_name] = v['timeUploadedMs']
 
             tag_list = utils.sort_dict(_tag_dict)
